4dd8af49fc4db2bed5203506710533889bbff057
yaahcd <cremaschi.yara@gmail.com>
2023-07-17 11:45:03 +0100

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import {BlogProvider }from './contexts/BlogContext'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
 
 
   return (
-    <>
-     
-    </>
+    <BlogProvider>
+      <Home />
+     <Routes>
+     </Routes>
+    </BlogProvider>
   )
 }
 
