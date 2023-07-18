16b90898ef428d0f4c1dd6d05414fa5ac9856705
yaahcd <cremaschi.yara@gmail.com>
2023-07-18 12:25:34 +0100

    ticket 8 done

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
-import { getCommentsByArticleId } from '../api';
+import { toast } from 'react-toastify';
+import { getCommentsByArticleId, postCommentsByArticleId } from '../api';
 import Loading from './Loading';
 
 function Comments({ id }) {
 	const [comments, setComments] = useState([]);
 	const [error, setError] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
+	const [newComment, setNewComment] = useState({
+    "username": '',
+    "body": ''
+  })
+  const [postConfirmation, setPostConfirmation] = useState(false)
 
 	useEffect(() => {
 		getCommentsByArticleId(id)
@@ -19,22 +25,41 @@ function Comments({ id }) {
 			});
 	}, []);
 
+	const handleSubmit = (e) => {
+    e.preventDefault();
+
+    postCommentsByArticleId(id, newComment).then((addedComment) => {
+			console.log(addedComment)
+      setComments([addedComment[0], ...comments])
+      setPostConfirmation(true)
+    }).catch((err) => {
+      setError(err)
+    })
+  }
+
 	if (error) {
-		return (
-			<section className="errorMessage">
-				<p>{error.request.status}: {error.response.data.msg}</p>
-			</section>
-		);
+		if (error.message === 'Network Error') {
+			toast.error('No connection');
+		} else {
+			return (
+				<section className="errorMessage">
+					<p>
+						{error.request.status}: {error.response.data.msg}
+					</p>
+				</section>
+			);
+		}
 	}
 
 	if (isLoading) {
 		return <Loading />;
 	}
 	return (
-		<ul className="commentList">
+		<>
+		<ul className="commentList" >
 			{comments.map((comment) => {
 				return (
-					<li className="commentItem" key={comment.comment_id}>
+					<li className="commentItem" key={Math.random()}>
 						<p>
 							{comment.author}: {comment.body}
 						</p>
@@ -43,6 +68,23 @@ function Comments({ id }) {
 				);
 			})}
 		</ul>
+		<section className='postComment'>
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="username"></label>
+			<input type="text" id='username' placeholder='Please add your username here' value={postConfirmation? '' : newComment.username} onChange={(e) => setNewComment((newComment) => {
+					return {...newComment, username: e.target.value }
+				})
+			} required/>
+			<label htmlFor="commentBox"></label>
+			<input type="text" id="commentBox" placeholder='Add your comment here...' value={postConfirmation? '' : newComment.body} onChange={(e) => setNewComment((newComment) => {
+					return {...newComment, body: e.target.value }
+				})
+				} required/>
+			<button className='btn' disabled={postConfirmation}>add comment</button>
+		</form>
+		{postConfirmation ? <p>Your comment has been posted.</p> : null}
+	</section>
+	</>
 	);
 }
 
