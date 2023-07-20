91007b5074d0a0667372f6be14908acc97d4b51f
yaahcd <cremaschi.yara@gmail.com>
2023-07-20 17:43:55 +0100

    chages to code

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -5,10 +5,6 @@ import BlogContext from "../contexts/BlogContext"
 function PrivateRoute() {
 
   const {user, setUser} = useContext(BlogContext)
-
-  if(window.localStorage.getItem('user')){
-    setUser(window.localStorage.getItem('user'))
-  }
   
   return user === window.localStorage.getItem('user') && user ? <Outlet /> : <Navigate to="/sign_in" />
 }
