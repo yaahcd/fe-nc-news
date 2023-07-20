d2292f8b4eedfe0d942edc28ac35e6fc20208acf
yaahcd <cremaschi.yara@gmail.com>
2023-07-20 17:45:10 +0100

    chages to code

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,15 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useContext, useState } from "react"
-import BlogContext from "../contexts/BlogContext"
+import { Navigate, Outlet } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import BlogContext from '../contexts/BlogContext';
 
 function PrivateRoute() {
+	const { user } = useContext(BlogContext);
 
-  const {user, setUser} = useContext(BlogContext)
-  
-  return user === window.localStorage.getItem('user') && user ? <Outlet /> : <Navigate to="/sign_in" />
+	return user === window.localStorage.getItem('user') && user ? (
+		<Outlet />
+	) : (
+		<Navigate to="/sign_in" />
+	);
 }
 
-export default PrivateRoute
+export default PrivateRoute;
