08583c9352b0b29be7605ede242c3a710f3baf60
Yara <cremaschi.yara@gmail.com>
2023-08-28 12:55:10 +0100

    updates to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SingleArticle from './components/SingleArticle';
-import ListOfArticles from './components/ListOfArticles';
-import PrivateRoute from './components/PrivateRoute';
-import MyAccount from './components/MyAccount';
-import SignIn from './components/SignIn';
-import UserHeader from './components/UserHeader';
-import Signup from './components/Signup';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { PageProvider } from "./context/PageContext";
+import ListOfArticles from "./components/ListOfArticles";
+import SingleArticle from "./components/SingleArticle";
 import InvalidPath from './components/InvalidPath';
-import { BlogProvider } from './contexts/BlogContext';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import UserList from './components/UserList';
-import PostArticle from './components/PostArticle';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+import SignIn from "./components/SignIn";
+import MyAccount from "./components/MyAccount";
+import SignUp from "./components/SignUp";
+import PostArticle from "./components/PostArticles";
+
 
 function App() {
-
-	return (
-		<BlogProvider>
-			<Navbar />
-			<UserHeader />
-			<Routes>
-				<Route path="/" element={<ListOfArticles />} />
-				<Route path="/articles/:article_id" element={<SingleArticle />} />
-				<Route path="/my_account" element={<PrivateRoute />}>
+  return (
+    <PageProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ListOfArticles />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route path="*" element={<InvalidPath />} />
+        <Route path="/my_account" element={<PrivateRoute />}>
 					<Route path="/my_account" element={<MyAccount />} />
 				</Route>
-				<Route path="/sign_in" element={<SignIn/>} />
-				<Route path="/sign_up" element={<Signup />} />
-				<Route path="/users" element={<UserList />} />
-				<Route path="/post_article" element={<PostArticle />} />
-				<Route path="*" element={<InvalidPath />} />
-			</Routes>
-			<ToastContainer />
-		</BlogProvider>
-	);
+        <Route path="/sign_in" element={<SignIn/>} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/post_article" element={<PostArticle />} />
+      </Routes>
+      <Footer/>
+    </PageProvider>
+  );
 }
 
 export default App;
