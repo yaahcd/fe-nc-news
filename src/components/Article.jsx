99b531f6125f167541512dcfe744ccec4e38f658
Yara <cremaschi.yara@gmail.com>
2023-08-28 12:52:08 +0100

    adding updates to single article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,23 +1,47 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ModeCommentIcon from "@mui/icons-material/ModeComment";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+TimeAgo.addDefaultLocale(en);
 
 function Article({ article }) {
-	return (
-		<li className="articleItem">
-			<Link to={`/articles/${article.article_id}`}>
-				<img
-					className="articleImg"
-					src={article.article_img_url}
-					alt={`Image for ${article.name}`}
-				/>
-			</Link>
-			<Link to={`/articles/${article.article_id}`}>
-				<h3>{article.title}</h3>
-			</Link>
-			<h4>Written by {article.author}</h4>
-			<p>Comments: {article.comment_count}</p>
-			<p>Votes: {article.votes}</p>
-		</li>
-	);
+  const navigate = useNavigate();
+
+  const timeAgo = new TimeAgo("en-US");
+  const createdAt = new Date(article.created_at);
+  const date = timeAgo.format(createdAt);
+
+  return (
+    <li
+      className="articleItem"
+      onClick={() => {
+        navigate(`/articles/${article.article_id}`);
+      }}
+    >
+      <img
+        className="articleImg"
+        src={article.article_img_url}
+        alt={`Image for ${article.name}`}
+      />
+
+      <h3>{article.title}</h3>
+
+      <h4>
+        Written by {article.author}, {date}
+      </h4>
+      <div className="iconsContainer">
+        <div className="comments">
+          <ModeCommentIcon className="commentIcon" />
+          <p> {article.comment_count} </p>
+        </div>
+        <div className="favorite">
+          <FavoriteIcon className="favoriteIcon" />
+          <p>{article.votes}</p>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default Article;
