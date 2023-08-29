725e477b1d1a1654d092eaf799d7625488c964e1
Yara <cremaschi.yara@gmail.com>
2023-08-29 08:44:16 +0100

    updates to SignUp component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,87 +1,105 @@
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import BlogContext from '../contexts/BlogContext';
-import Loading from './Loading';
-import { postUser } from '../api';
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
+import { postUser } from "../api";
+import PageContext from "../context/PageContext";
+import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
+import Error from "./Error";
 
-function Signup() {
-	const [newUser, setNewUser] = useState({
-		username: '',
-		name: '',
-		avatar_url: '',
-	});
-	const [error, setError] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
-	const { user, setUser } = useContext(BlogContext);
-	const navigate = useNavigate();
+function SignUp() {
+  const [newUser, setNewUser] = useState({
+    username: "",
+    name: "",
+    avatar_url: "",
+  });
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { user, setUser } = useContext(PageContext);
+  const navigate = useNavigate();
+  const lightTheme = useSelector((state) => state.themeKey);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setIsLoading(true);
-		postUser(newUser)
-			.then(({ postedUser }) => {
-				setIsLoading(false);
-				setUser(postedUser[0].username);
-				navigate('/my_account');
-			})
-			.catch((err) => {
-				setError(true);
-				setIsLoading(false);
-			});
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    postUser(newUser)
+      .then(({ postedUser }) => {
+        setIsLoading(false);
+        setUser(postedUser[0].username);
+        navigate("/my_account");
+      })
+      .catch((err) => {
+        setError(true);
+        setIsLoading(false);
+      });
+  };
 
-	if (error) {
-		return (
-			<p className="errorMessage">Something went wrong. Please try again.</p>
-		);
-	}
+  if (error) {
+    return <Error />
+  }
 
-	if (isLoading) {
-		return <Loading />;
-	}
+  if (isLoading) {
+    return <Loading />;
+  }
 
-	return (
-		<form onSubmit={handleSubmit} className="signUpForm">
-			<h2>Hey there!</h2>
-			<input
-				id="username"
-				type="text"
-				value={newUser.username}
-				placeholder="Enter your username"
-				onChange={(e) =>
-					setNewUser((newUser) => {
-						return { ...newUser, username: e.target.value };
-					})
-				}
-				required
-			/>
-			<input
-				id="name"
-				type="text"
-				value={newUser.name}
-				placeholder="Enter your name"
-				onChange={(e) =>
-					setNewUser((newUser) => {
-						return { ...newUser, name: e.target.value };
-					})
-				}
-				required
-			/>
-			<input
-				id="avatar_url"
-				type="text"
-				value={newUser.avatar_url}
-				placeholder="Enter the URL for your profile image"
-				onChange={(e) =>
-					setNewUser((newUser) => {
-						return { ...newUser, avatar_url: e.target.value };
-					})
-				}
-				required
-			/>
-			<button className="btn">Sign-up</button>
-		</form>
-	);
+  return (
+    <main className={"signUpContainer" + (lightTheme ? "" : " dark")}>
+      <form onSubmit={handleSubmit} className="signUpForm">
+        <h2>Hey there!</h2>
+        <input
+          id="username"
+          type="text"
+          value={newUser.username}
+          placeholder="Enter your username"
+          onChange={(e) =>
+            setNewUser((newUser) => {
+              return { ...newUser, username: e.target.value };
+            })
+          }
+          required
+        />
+        <input
+          id="name"
+          type="text"
+          value={newUser.name}
+          placeholder="Enter your name"
+          onChange={(e) =>
+            setNewUser((newUser) => {
+              return { ...newUser, name: e.target.value };
+            })
+          }
+          required
+        />
+        <input
+          id="avatar_url"
+          type="text"
+          value={newUser.avatar_url}
+          placeholder="Enter the URL for your profile image"
+          onChange={(e) =>
+            setNewUser((newUser) => {
+              return { ...newUser, avatar_url: e.target.value };
+            })
+          }
+          required
+        />
+        <Button
+          type="submit"
+          size="small"
+          variant="contained"
+          className="btn"
+          sx={{
+            color: "white",
+            backgroundColor: "grey",
+            height: "20px",
+            borderRadius: "15px",
+            ":hover": { backgroundColor: "#D90429" },
+          }}
+        >
+          Sign up
+        </Button>
+      </form>
+    </main>
+  );
 }
 
-export default Signup;
+export default SignUp;
