cb50fcd4a8ec36e2f7bed7c5a1a1698210099d49
Yara <cremaschi.yara@gmail.com>
2023-08-29 08:40:46 +0100

    updates to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,69 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import logo from "../assets/ncnewslogo.png";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { IconButton } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../features/themeSlice";
+import PageContext from "../context/PageContext";
 
 function Navbar() {
-	return (
-		<>
-			<nav className="navbar">
-			<Link to="/">
-				<h1 className="header"> NC News</h1>
-			</Link>
-				<ul>
-					<Link to="/">
-						<li>Home</li>
-					</Link>
-					<Link to="/post_article">
-						<li>New Article</li>
-					</Link>
-					<Link to="/users">
-						<li>Users</li>
-					</Link>
-					<Link to="/my_account">
-						<li>My Account</li>
-					</Link>
-				</ul>
-			</nav>
-			</>
-	);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const lightTheme = useSelector((state) => state.themeKey);
+  const { user } = useContext(PageContext);
+
+  if (!lightTheme) {
+    document.getElementById("themeToggle").className = "dark";
+  } else {
+    document.getElementById("themeToggle").className = "";
+  }
+
+  return (
+    <>
+      <img
+        className="logo"
+        src={logo}
+        alt=""
+        onClick={() => {
+          navigate("/");
+        }}
+      />
+      <nav className={"navbar" + (lightTheme ? "" : " dark")}>
+        <IconButton
+          className={"themeButton" + (lightTheme ? "" : " dark")}
+          onClick={() => {
+            dispatch(toggleTheme());
+          }}
+        >
+          {lightTheme && <LightModeIcon />}
+          {!lightTheme && <DarkModeIcon />}
+        </IconButton>
+        <ul className={"navOptions" + (lightTheme ? "" : " dark")}>
+          <Link to="/" className="noShow">
+            <li>Home</li>
+          </Link>
+          <Link to="/post_article">
+            <li>New Article</li>
+          </Link>
+          <Link to="/my_account">
+            <li>My Account</li>
+          </Link>
+          {user ? (
+            <Link className="lastItem noShow">
+              {" "}
+              <li>Hello {user}!</li>
+            </Link>
+          ) : (
+            <Link className="lastItem noShow" to="/sign_in">
+              <li>Sign in</li>
+            </Link>
+          )}
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
