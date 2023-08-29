e59da84e5838b79fbd1b2290a1c10014b8cd52fb
Yara <cremaschi.yara@gmail.com>
2023-08-29 08:41:27 +0100

    updates to postArticle component

--- src/components/PostArticle.jsx
+++ src/components/PostArticle.jsx
@@ -1,123 +1,206 @@
-import { useState, useContext, useEffect } from 'react';
-import { getTopics, postArticle, postTopic } from '../api';
-import Loading from './Loading';
-import BlogContext from '../contexts/BlogContext';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState, useContext, useEffect } from "react";
+import { getTopics, postArticle, postTopic } from "../api";
+import Loading from "./Loading";
+import PageContext from "../context/PageContext";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import { useSelector } from "react-redux";
+import Error from "./Error";
 
 function PostArticle() {
-	const [topicsList, setTopicsList] = useState();
-	const [error, setError] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
-	const { user } = useContext(BlogContext);
-  const navigate = useNavigate()
-	const [newPost, setNewPost] = useState({
-		title: '',
-		topic: '',
-		body: '',
-		article_img_url: '',
-		author: user,
-	});
+  const [topicsList, setTopicsList] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [topicButtonClicked, setTopicButtonClicked] = useState(false);
+  const { user } = useContext(PageContext);
+  const navigate = useNavigate();
+  const [newPost, setNewPost] = useState({
+    title: "",
+    topic: "",
+    body: "",
+    article_img_url: "",
+    author: user,
+  });
+  const lightTheme = useSelector((state) => state.themeKey);
 
-	useEffect(() => {
-		setError(false);
-		getTopics()
-			.then((topics) => {
-				setTopicsList([...topics]);
-				setIsLoading(false);
-			})
-			.catch((err) => {
-				setError(err);
-				setIsLoading(false);
-			});
-	}, []);
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
- 
-    setIsLoading(true)
-    const topicListFilter = topicsList.filter((topic) => topic.slug === newPost.topic)
-
-    if(topicListFilter.length === 0){
-      postTopic(newPost.topic).then((newTopic) => {
-        setTopicsList([...newTopic, topicsList])
-      }).catch((err) => {
-        setError(true)
+  useEffect(() => {
+    setError(false);
+    getTopics()
+      .then((topics) => {
+        setTopicsList([...topics]);
+        setIsLoading(false);
       })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+    const topicListFilter = topicsList.filter(
+      (topic) => topic.slug === newPost.topic
+    );
+
+    if (topicListFilter.length === 0) {
+      postTopic(newPost.topic)
+        .then((newTopic) => {
+          setTopicsList([...newTopic, topicsList]);
+        })
+        .catch((err) => {
+          setError(true);
+        });
     }
 
-    postArticle(newPost).then(({article_posted}) => {
-      setIsLoading(false)
-      navigate(`/articles/${article_posted[0].article_id}`)
-    }).catch((err) => {
-      setError(true)
-      setIsLoading(false)
-    })
-	};
+    postArticle(newPost)
+      .then(({ article_posted }) => {
+        setIsLoading(false);
+        navigate(`/articles/${article_posted[0].article_id}`);
+      })
+      .catch((err) => {
+        setError(true);
+        setIsLoading(false);
+      });
+  };
 
-  
-	if (error) {
-		return (
-			<p className="errorMessage">Something went wrong. Please try again.</p>
-		);
-	}
+  if (error) {
+    return <Error />
+  }
 
-	if (isLoading) {
-		return <Loading />;
-	}
-	return user ? <main className="postArticleContainer">
-			<h2>Post new article</h2>
-			<form onSubmit={handleSubmit} className="postForm">
-        <label htmlFor="title">Article title:</label>
-				<input
-					type="text"
-					id="title"
-					value={newPost.title}
-					onChange={(e) =>
-						setNewPost((newPost) => {
-							return { ...newPost, title: e.target.value };
-						})
-					}
-					required
-				/>
-				<section className="topicSectionForm">
-					<label htmlFor="topic">Topic:</label>
-					<input
-						id="topic"
-						type="text"
-						value={newPost.topic}
-						onChange={(e) =>
-							setNewPost((newPost) => {
-								return { ...newPost, topic: e.target.value.toLowerCase() };
-							})
-						}
-						required
-					/>
-				</section>
-				<textarea
-					type="text"
-					id="body"
-          placeholder='What is on you mind?'
-					onChange={(e) =>
-						setNewPost((newPost) => {
-							return { ...newPost, body: e.target.value };
-						})
-					}
-				></textarea>
-        <label htmlFor="topic">URL image for your article:</label>
-				<input
-					type="text"
-					id="url"
-					value={newPost.article_img_url}
-					onChange={(e) =>
-						setNewPost((newPost) => {
-							return { ...newPost, article_img_url: e.target.value };
-						})
-					}
-				/>
-				<button className="btn">Post</button>
-			</form>
-		</main> 
-    : <p className='PostArticleError'>Please <Link to='/sign_in'>sign-in</Link> to post an article.</p>
+  if (isLoading) {
+    return <Loading />;
+  }
+  return user ? (
+    <main className={"postArticleContainer" + (lightTheme ? "" : " dark")}>
+      <h2>Post new article</h2>
+
+      <form onSubmit={handleSubmit} className="postForm">
+        <section className={"postTitle" + (lightTheme ? "" : " dark")}>
+          <label htmlFor="title">Article title:</label>
+          <input
+            type="text"
+            id="title"
+            value={newPost.title}
+            onChange={(e) =>
+              setNewPost((newPost) => {
+                return { ...newPost, title: e.target.value };
+              })
+            }
+            required
+          />
+        </section>
+
+        <section className={"postSelection" + (lightTheme ? "" : " dark")}>
+          <label htmlFor="">Topics:</label>
+          <select
+            id="topic"
+            type="text"
+            onChange={(e) =>
+              setNewPost((newPost) => {
+                return { ...newPost, topic: e.target.value };
+              })
+            }
+          >
+            {topicsList
+              ? topicsList.map((topic) => {
+                  return (
+                    <option key={topic.slug} value={topic.slug}>
+                      {topic.slug}
+                    </option>
+                  );
+                })
+              : null}
+          </select>
+        </section>
+
+        <section className="addTopicButton">
+          <Button
+            size="small"
+            variant="contained"
+            sx={{
+              color: "white",
+              backgroundColor: "grey",
+              height: "25px",
+              borderRadius: "15px",
+              ":hover": { backgroundColor: "#D90429" },
+            }}
+            onClick={() => {
+              setTopicButtonClicked(!topicButtonClicked);
+              newPost.topic = "";
+            }}
+          >
+            Want to add a new topic?
+          </Button>
+        </section>
+
+        {topicButtonClicked && (
+          <section className={"postAddTopic" + (lightTheme ? "" : " dark")}>
+            <label htmlFor="topic">New topic:</label>
+            <input
+              id="topic"
+              type="text"
+              value={newPost.topic}
+              onChange={(e) =>
+                setNewPost((newPost) => {
+                  return {
+                    ...newPost,
+                    topic: e.target.value.toLowerCase().trim(),
+                  };
+                })
+              }
+              required
+            />
+          </section>
+        )}
+
+        <textarea
+          required
+          type="text"
+          id="body"
+          placeholder="What is on you mind?"
+          onChange={(e) =>
+            setNewPost((newPost) => {
+              return { ...newPost, body: e.target.value };
+            })
+          }
+        ></textarea>
+
+        <section className={"postArticleImg" + (lightTheme ? "" : " dark")}>
+          <label htmlFor="topic">URL image:</label>
+          <input
+            type="text"
+            id="url"
+            value={newPost.article_img_url}
+            onChange={(e) =>
+              setNewPost((newPost) => {
+                return { ...newPost, article_img_url: e.target.value };
+              })
+            }
+          />
+        </section>
+
+        <section className="postArticleButton">
+          <Button
+            type="submit"
+            size="small"
+            variant="contained"
+            sx={{
+              color: "white",
+              backgroundColor: "grey",
+              height: "25px",
+              borderRadius: "15px",
+              ":hover": { backgroundColor: "#D90429" },
+            }}
+          >
+            Post article
+          </Button>
+        </section>
+      </form>
+    </main>
+  ) : <Error message={"Please sign-in to post an article."}/>
+    
 }
 
 export default PostArticle;
