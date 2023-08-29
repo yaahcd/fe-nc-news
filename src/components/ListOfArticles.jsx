250c9ce41f62b39ac8757f4868d90865c99b56d1
Yara <cremaschi.yara@gmail.com>
2023-08-29 08:32:50 +0100

    updates to listOfArticles component

--- src/components/ListOfArticles.jsx
+++ src/components/ListOfArticles.jsx
@@ -1,50 +1,134 @@
-import { useEffect, useState } from 'react';
-import { getArticles } from '../api';
-import Article from './Article';
-import Loading from './Loading';
-import SearchBar from './SearchBar';
+import { useEffect, useState } from "react";
+import { getArticles } from "../api";
+import { useSearchParams } from "react-router-dom";
+import Error from "./Error";
+import SingleArticle from "./ArticleItem";
+import SearchBar from "./SearchBar";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import Loading from "./Loading";
+import { useSelector } from "react-redux";
 
 function ListOfArticles() {
-	const [articlesList, setArticlesList] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [error, setError] = useState(false);
+  const [articlesList, setArticlesList] = useState([]);
+  const [noQueryArticleList, setNoQueryArticleList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [error, setError] = useState(false);
+  const [pageChange, setPageChange] = useState(false)
+  const [resetFilter, setResetFilter] = useState(false)
+  const lightTheme = useSelector((state) => state.themeKey);
+  let page = searchParams.get("p");
 
-	useEffect(() => {
-		getArticles()
-			.then(({ articles }) => {
-				setArticlesList(articles);
-				setIsLoading(false);
-			})
-			.catch((err) => {
-				setError(err);
-				setIsLoading(false);
-			});
-	}, []);
+  
+  useEffect(() => {
+    page
+      ? page
+      : (page = setSearchParams((current) => {
+          current.set("p", 1);
+          return current;
+        }));
+        
+    getArticles(undefined, undefined, undefined, 6, +page)
+      .then(({ articles }) => {
+        setArticlesList(articles);
+        setNoQueryArticleList(articles);
+        setIsLoading(false);
+        setPageChange(false);
+      })
+      .catch((err) => {
+        setError(true);
+        setIsLoading(false);
+      });
+  }, [pageChange]);
 
-	if (error) {
-		return (
-			<p className="errorMessage"> Something went wrong. Please try again.</p>
-		);
-	}
+  const handleClick = (e) => {
+    setSearchParams((current) => {
+      current.set("p", +e.target.innerText);
+      return current;
+    });
+    setPageChange(true);
+  };
 
-	if (isLoading) {
-		return <Loading />;
-	}
+  const handleArrowClick = (side) => {
+    if (side === "left") {
+      setSearchParams((current) => {
+        current.set("p", +page - 1);
+        return current;
+      });
+      setPageChange(true);
+    } else {
+      setSearchParams((current) => {
+        current.set("p", +page + 1);
+        return current;
+      });
+      setPageChange(true);
+    }
+  };
 
-	return (
-		<>
-			<SearchBar setArticlesList={setArticlesList} />
-			<ul className="articlesList">
-				{articlesList.length === 0 ? (
-					<p>No articles on this topic.</p>
-				) : (
-					articlesList.map((article) => {
-						return <Article key={article.article_id} article={article} />;
-					})
-				)}
-			</ul>
-		</>
-	);
+  if (error) {
+    return <Error />;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <SearchBar setArticlesList={setArticlesList} />
+      <ul className="articlesList">
+        {articlesList.length === 0 ? (
+          <p>No articles on this topic.</p>
+        ) : (
+          articlesList.map((article, index) => {
+            return (
+              <SingleArticle
+                key={index}
+                article={article}
+                date={article.created_at}
+              />
+            );
+          })
+        )}
+      </ul>
+      <div className={"paginationArray" + (lightTheme ? "" : " dark")}>
+        {noQueryArticleList.length === articlesList.length && (
+          <>
+            <button
+              onClick={() => {
+                handleArrowClick("left");
+              }}
+              disabled={+page === 1}
+            >
+              <KeyboardArrowLeftIcon />
+            </button>
+            {Array(7)
+              .fill("")
+              .map((_, index) => {
+                return (
+                  <button
+                    onClick={handleClick}
+                    className={+page === index + 1 ? "boldNumber" : null}
+                    key={index}
+                  >
+                    {index + 1}
+                  </button>
+                );
+              })}
+            <button
+              onClick={() => {
+                handleArrowClick("right");
+              }}
+              disabled={+page === 7}
+            >
+              <KeyboardArrowRightIcon />
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ListOfArticles;
