4e826c1195df1cfc3ee82bab941778064616b31e
Yara <cremaschi.yara@gmail.com>
2023-08-29 08:34:39 +0100

    updates to Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,25 @@
 function Loading() {
 	return (
-		<section className="loadingSpinnerContainer">
-			<section className="loadingSpinner"></section>
-		</section>
+	  <div className="loadingWheel">
+		<div className="loadingio-spinner-spinner-1axg0m9izkd">
+		  <div className="ldio-cq6l1bzvz1e">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		  </div>
+		</div>
+	  </div>
 	);
-}
-
-export default Loading;
+  }
+  
+  export default Loading;
+  
